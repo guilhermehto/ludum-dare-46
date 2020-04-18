6a1e597b6200be9b466b936d76ef46fe8abf8b3d
Guilherme Oliveira <htoguilherme@gmail.com>
2020-04-18 14:36:48 +1200

    Player movement

---
+++ actors/player/Player.gd
@@ -0,0 +1,19 @@
+extends KinematicBody
+
+export var move_speed := 15.0
+
+func _physics_process(_delta: float) -> void:
+	var horizontal_movement := int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	var vertical_movement := int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
+
+	var movement_vector = Vector3(horizontal_movement, 0, vertical_movement).normalized()
+
+	move_and_slide(movement_vector * move_speed, Vector3.UP)
+	look_at(get_mouse_position(), Vector3.UP)
+
+func get_mouse_position() -> Vector3:
+	var camera = get_viewport().get_camera()
+	var depth = Vector3.ZERO.distance_to(camera.translation)
+	var mouse_position = camera.project_position(get_viewport().get_mouse_position(), depth)
+	mouse_position.y = translation.y
+	return mouse_position
