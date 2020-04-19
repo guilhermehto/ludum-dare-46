d49c8f280a540bd58d0108d1fd5f4a05abe0a0dc
Guilherme Oliveira <htoguilherme@gmail.com>
2020-04-19 20:16:18 +1200

    Added shit animations

--- actors/player/Player.gd
+++ actors/player/Player.gd
@@ -6,6 +6,7 @@ export var move_speed := 15.0
 onready var inventory = $Inventory
 onready var warm_body = $WarmBody
 onready var hands = $Hands
+onready var animation_player = $PlayerMesh/Armature/AnimationPlayer
 
 func _physics_process(_delta: float) -> void:
 	var horizontal_movement := int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
@@ -14,7 +15,15 @@ func _physics_process(_delta: float) -> void:
 	var movement_vector = Vector3(horizontal_movement, 0, vertical_movement).normalized()
 
 	move_and_slide(movement_vector * move_speed, Vector3.UP)
-	look_at(get_mouse_position(), Vector3.UP)
+	
+	if movement_vector != Vector3.ZERO:
+		if animation_player.current_animation != "Run":
+			animation_player.play("Run")
+		look_at(global_transform.origin + movement_vector * 3, Vector3.UP)
+	else:
+		if animation_player.current_animation != "Idle":
+			animation_player.play("Idle")
+		
 
 func get_mouse_position() -> Vector3:
 	var camera = get_viewport().get_camera()
@@ -22,3 +31,7 @@ func get_mouse_position() -> Vector3:
 	var mouse_position = camera.project_position(get_viewport().get_mouse_position(), depth)
 	mouse_position.y = translation.y
 	return mouse_position
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	if anim_name != "Hit":
+		animation_player.play(anim_name)
