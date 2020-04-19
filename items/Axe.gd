0a45f9b961c9783451abb60160cc8755dac937a8
Guilherme Oliveira <htoguilherme@gmail.com>
2020-04-20 00:55:51 +1200

    Added axe durability

--- items/Axe.gd
+++ items/Axe.gd
@@ -7,6 +7,7 @@ onready var hit_timer : Timer = $HitTime
 onready var hitbox : Area = $Area/CollisionShape
 
 export var damage : int = 15
+export var hits : int = 10
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("primary_action") and cooldown.is_stopped():
@@ -14,6 +15,13 @@ func _input(event: InputEvent) -> void:
 		hit_timer.start()
 		hitbox.disabled = false
 		emit_signal("hit")
+		hits -= 1
+		if hits == 0:
+			brake_axe()
+
+func brake_axe() -> void:
+	GlobalSignals.emit_notified("Your axe broke")
+	queue_free()
 
 func _on_Area_body_entered(body: Node) -> void:
 	if not hit_timer.is_stopped():
